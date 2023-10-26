e32c40eaf8d55b643e80b950ff97f9e495b9d546
Maxsys <letrandangkhoadl@gmail.com>
2023-10-26 17:41:57 +0700

    Initial commit

---
+++ app/src/main/java/edu/uef/thithuchanh/MainActivity.java
@@ -0,0 +1,106 @@
+package edu.uef.thithuchanh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static class View_An_Item
+    {
+        public ImageView imageview;
+        public TextView textview;
+    }
+
+    public class FoodAdapter extends BaseAdapter
+    {
+        Context context;
+        public FoodAdapter(Context c)
+        {
+            context=c;
+        }
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return layoutIcon.length;
+        }
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return layoutIcon[position];
+        }
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            // TODO Auto-generated method stub
+            View_An_Item an_item;
+            LayoutInflater layoutinflater= ((Activity)context).getLayoutInflater();
+            if(arg1==null)
+            {
+                an_item = new View_An_Item();
+                arg1 = layoutinflater.inflate(R.layout.list_item_icon, null);
+                an_item.textview = (TextView) arg1.findViewById(R.id.textView1);
+                an_item.imageview = (ImageView)arg1.findViewById(R.id.imageView1);
+                arg1.setTag(an_item);
+            }
+            else
+                an_item =(View_An_Item)arg1.getTag();
+            an_item.imageview.setImageResource(layoutIcon[arg0]);
+            an_item.textview.setText(layoutName[arg0]);
+            return arg1;
+        }
+    }
+
+
+    public ListView lv;
+    static String[] layoutName;
+    int dem = 0;
+
+    static int[] layoutIcon={R.drawable.bread,R.drawable.cherrycheesecake,R.drawable.gingerbreadhouse
+            ,R.drawable.hamburger,R.drawable.sunnysideupeggs,R.drawable.sunnysideupeggs
+            ,R.drawable.hamburger,R.drawable.gingerbreadhouse,R.drawable.cherrycheesecake,R.drawable.bread
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutName = getResources().getStringArray(R.array.layout_name);
+
+        lv = (ListView) findViewById(R.id.listViewMain);
+//        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(this, R.layout.list_item,
+//                layoutName);
+//        lv.setAdapter(mAdapter);
+        lv.setAdapter(new FoodAdapter(this));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position
+                    , long l) {
+//                TextView tv_selected = (TextView) view;
+//                Toast.makeText(getApplicationContext(), tv_selected.getText(),
+//                        Toast.LENGTH_SHORT).show();
+                String itemList = layoutName[position];
+                Toast.makeText(MainActivity.this, "Clicked "+ itemList+" at Position: "+(position+1),
+                        Toast.LENGTH_SHORT).show();
+
+                int vitribam = position; dem++;
+                Intent intent = new Intent (MainActivity.this,ShowListActivity.class);
+                intent.putExtra("ViTriChon", vitribam);
+                intent.putExtra("dem", dem);
+                startActivity(intent);
+            }
+        });
+    }
+}
